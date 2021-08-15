8ea5680f334958950173d37e3dad78fc0b6a7278
AlexShemyakin <64311224+AlexShemyakin@users.noreply.github.com>
2021-08-15 17:56:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import sys
 var = sys.argv[1]
 
 doc = xml.dom.minidom.parse(var)
-path = doc.getElementsByTagName("file")
+file = doc.getElementsByTagName("file")
 
-for count in range(len(path)):
-    shutil.copy(os.path.join(path[count].getAttribute("source_path"), path[count].getAttribute("file_name")),
-                path[count].getAttribute("destination_path"))
+for count in range(len(file)):
+    shutil.copy(os.path.join(file[count].getAttribute("source_path"), file[count].getAttribute("file_name")),
+                file[count].getAttribute("destination_path"))
 
