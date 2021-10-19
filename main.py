ff9dc267fe2b9650c734980589aa6f9f5b043c25
AlexShemyakin <64311224+AlexShemyakin@users.noreply.github.com>
2021-10-19 20:56:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import shutil
 import os
 import sys
 
-var = sys.argv[1]
+def copy(var):
+    doc = xml.dom.minidom.parse(var)
+    file = doc.getElementsByTagName("file")
 
-doc = xml.dom.minidom.parse(var)
-file = doc.getElementsByTagName("file")
+    for count in range(len(file)):
+        shutil.copy(os.path.join(file[count].getAttribute("source_path"), file[count].getAttribute("file_name")),
+                    file[count].getAttribute("destination_path"))
 
-for count in range(len(file)):
-    shutil.copy(os.path.join(file[count].getAttribute("source_path"), file[count].getAttribute("file_name")),
-                file[count].getAttribute("destination_path"))
-
+if __name == '__main__':
+    copy(sys.argv[1])
